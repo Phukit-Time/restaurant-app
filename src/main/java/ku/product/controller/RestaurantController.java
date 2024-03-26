bfae2e0e68db5d1783c7e03cbbbed81af86e9f63
Phukit-Time <phukit.s@ku.th>
2024-03-27 01:27:49 +0700

    New project to push to GitHub

---
+++ src/main/java/ku/product/controller/RestaurantController.java
@@ -0,0 +1,17 @@
+package ku.product.controller;
+
+import ku.product.entity.Product;
+import ku.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+
+public class RestaurantController {
+
+}
